bb3d9b95dec21e7d26db60fd83be58e90dfee785
papanisaicharan <charansai850@gmail.com>
2021-02-09 17:17:59 +0530

    Chapter 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
 import "./style.css";
 
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>Saicharan's Blog</h1>
+      <h1>{props.name}'s Blog</h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <h3>Learn how i am made</h3>
+      <h3>{props.adjective}</h3>
+      <ul style={{ textAlign: "left" }}>
+        {props.lang.map((lan, index) => (
+          <li key={index}>{lan}</li>
+        ))}
+      </ul>
     </section>
   );
 }
 
-function Footer() {
+function Main1(props) {
+  return (
+    <section>
+      <h3>{props.adjective}</h3>
+      <ul style={{ textAlign: "left" }}>
+        {props.lang.map(language => (
+          <li key={language.id}>{language.language}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+const languages = ["c", "c++", "java", "python"];
+const languageObjects = languages.map((lan, i) => ({ id: i, language: lan }));
+console.log(languageObjects);
+
+function Footer(props) {
   return (
     <footer>
-      <h5>footer</h5>
+      <h5>Copyright {props.year}</h5>
     </footer>
   );
 }
@@ -28,9 +49,10 @@ function Footer() {
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main/>
-      <Footer/>
+      <Header name="Saicharan Papani" />
+      <Main adjective="Passing from main" lang={languages} />
+      <Main1 adjective="Repeat with keys" lang={languageObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
